89a7d66b88865870bf0d6ce344c3193fb44e5801
tomnvt <tom.novota@gmail.com>
2018-02-24 21:45:39 +0100

    First commit

---
+++ SimpleSynth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleSynth
+//
+//  Created by NVT on 24.02.18.
+//  Copyright © 2018 NVA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
