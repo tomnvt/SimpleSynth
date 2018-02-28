dadc5fa0da1439e1f28937a61bd5580025f6b3ca
tomnvt <tom.novota@gmail.com>
2018-02-28 16:53:47 +0100

    Change PianoView to AKKeyboardView

--- SimpleSynth/ViewController.swift
+++ SimpleSynth/ViewController.swift
@@ -7,23 +7,26 @@
 //
 
 import UIKit
-import PianoView
 import AudioKit
+import AudioKitUI
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
-    @IBOutlet weak var pianoView: PianoView!
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, AKKeyboardDelegate {
+
     @IBOutlet weak var noteLabel: UILabel!
     @IBOutlet weak var wave1Picker: UIPickerView!
+    @IBOutlet weak var keyboardView: UIView!
     
     var currentMIDINote: MIDINoteNumber = 0
     
     var oscillator : AKOscillator?
     
-    let notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C"]
+    var currentAmplitude = 0.1
+    var currentRampTime = 0.0
     
     let waveforms = [AKTable(.square, count: 256), AKTable(.triangle, count: 256), AKTable(.sine, count: 256), AKTable(.sawtooth, count: 256)]
     let waveformNames = ["square", "triangle", "sine", "sawtooth"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,9 +34,16 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         setOscillator(waveformIndex: 0)
         
         AudioKit.start()
+        
+        AudioKit.output = oscillator
 
         wave1Picker.dataSource = self
         wave1Picker.delegate = self
+        
+        let keyboard = AKKeyboardView(width: Int(keyboardView.frame.size.width), height: Int(keyboardView.frame.size.height), firstOctave: 3, octaveCount: 2)
+        keyboard.delegate = self
+        keyboardView.addSubview(keyboard)
+        
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -41,7 +51,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return waveformNames.count;
+        return waveformNames.count
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
@@ -56,6 +66,23 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         setOscillator(waveformIndex: row)
     }
+    func noteOn(note: MIDINoteNumber) {
+        currentMIDINote = note
+        if oscillator?.amplitude == 0 {
+            oscillator?.rampTime = 0
+        }
+        oscillator?.frequency = note.midiNoteToFrequency()
+        
+        oscillator?.rampTime = currentRampTime
+        oscillator?.amplitude = currentAmplitude
+        oscillator?.play()
+    }
+    
+    func noteOff(note: MIDINoteNumber) {
+        if currentMIDINote == note {
+            oscillator?.amplitude = 0
+        }
+    }
     
     func setOscillator(waveformIndex: Int) {
         oscillator = AKOscillator(waveform: waveforms[waveformIndex])
@@ -64,17 +91,6 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         oscillator?.amplitude = 0.05
     }
     
-    @IBAction func keyButtonPressed(_ sender: UIButton) {
-        noteLabel.text = "\(notes[sender.tag]) pressed"
-        oscillator?.frequency = ((sender.tag)+60).midiNoteToFrequency()
-        oscillator?.play()
-    }
-    
-    
-    @IBAction func keyButtonReleased(_ sender: UIButton) {
-        noteLabel.text = "\(notes[sender.tag]) released"
-        oscillator?.stop()
-    }
     
 }
 
